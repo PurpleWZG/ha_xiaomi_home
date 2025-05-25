c98594b83bac561b37daeaa630fe62a4b230fd5b
PurpleWZG <2927057406@qq.com>
2025-05-25 17:13:10 +0800

    fixxiaomi

--- custom_components/xiaomi_home/text.py
+++ custom_components/xiaomi_home/text.py
@@ -45,7 +45,7 @@ off Xiaomi or its affiliates' products.
 
 Text entities for Xiaomi Home.
 """
-from __future__ import annotations
+from __future__ import annotation
 import logging
 from typing import Any, Optional
 
